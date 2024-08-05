9435c188d9e6fab52bb6717e8c7258ceb63f95f9
new-college <omkarpatil98986029@gmail.com>
2024-08-05 16:16:01 +0530

    Task and projects

---
+++ airbnb/src/Component/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src='./Airbnbimages/Airbnblogo.png' alt='' className='nav--logo'></img>
+    </nav>
+  )
+}
+
+export default Navbar
